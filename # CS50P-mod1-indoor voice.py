58dea87127b60a141f3a78917343f3973e2fb2ac
ssdhilon <67072941+ssdhilon@users.noreply.github.com>
2025-12-15 15:09:33 -0800

    deleting ipynb file and upload py

---
+++ # CS50P-mod1-indoor voice.py
@@ -0,0 +1,7 @@
+# CS50P - Module1
+# Problem - indoor voice
+# Prompt user for name and print name as lower case preserving all white spaces
+
+#indoor voice
+name = str(input("what's your name? "))
+print(f'Your name is {name.lower()}')
