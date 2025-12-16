5753d99184ba74fe4cda33d2b6040f6bc43fd5ad
ssdhilon <67072941+ssdhilon@users.noreply.github.com>
2025-12-16 07:35:10 -0800

    trying to push changes

--- indoor.py
+++
@@ -1,7 +0,0 @@
-# CS50P - Module1
-# Problem - indoor voice
-# Prompt user for name and print name as lower case preserving all white spaces
-
-#indoor voice
-name = str(input("What's your name? "))
-print(name.lower())
